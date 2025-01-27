9a3bc034a1af2bec96fde03370e26fd91302b5d6
pedro <pedro092692@gmail.com>
2025-01-27 18:40:41 +0100

    add function to select audio file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var gamePattern = [];
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
+var userClickedPatter = [];
 var randomChosenColour = buttonColors[nextSequence()];
 gamePattern.push(randomChosenColour);
 
@@ -9,4 +10,18 @@ function nextSequence(){
     return randomNumber;
 }
 
-console.log(gamePattern);
+// generate audop object 
+function audiodObj(color){
+    return new Audio('./sounds/' + color + '.mp3');
+}
+
+$(document).on('keydown', function(event){
+    if(event.key === 't'){
+        var selectedColor = $('.' + randomChosenColour);
+        var sound = audiodObj(randomChosenColour);
+        sound.play();
+        selectedColor.animate({opacity: 0}, 70).animate({opacity: 1}, 70)
+
+    }
+});
+
