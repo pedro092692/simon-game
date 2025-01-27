43dff4f5f53cac9b468d6c33e8a67b8de72a275e
pedro <pedro092692@gmail.com>
2025-01-27 21:14:55 +0100

    refactor nextSequence function

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 var gamePattern = [];
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var userClickedPatter = [];
-var randomChosenColour = buttonColors[nextSequence()];
-gamePattern.push(randomChosenColour);
+
 
 //generate random number 
 function nextSequence(){
     var randomNumber =  Math.floor(Math.random() * 4);
-    return randomNumber;
-}
-
-// generate audop object 
-function audiodObj(color){
-    return new Audio('./sounds/' + color + '.mp3');
+    var randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+    var lastColor = gamePattern.slice(-1);
+    //play sound
+    playSound(lastColor);
+    //animate
+    animateButton($('#' + lastColor));
 }
 
 //get clicked buttons
 $('.btn').on('click', function(){
     var btnId = this.id;
+    var button = $('#' + btnId);
     userClickedPatter.push(btnId)
-    console.log(userClickedPatter);
+    //play sound 
+    playSound(btnId);
+    //animate button 
+    animateButton(button);
 });
 
-$(document).on('keydown', function(event){
-    if(event.key === 't'){
-        var selectedColor = $('.' + randomChosenColour);
-        var sound = audiodObj(randomChosenColour);
-        sound.play();
-        selectedColor.animate({opacity: 0}, 70).animate({opacity: 1}, 70)
+//function play sound
+function playSound(soundName){
+    var sound = audiodObj(soundName);
+    sound.play();
+}
 
-    }
-});
+//function animate button
+function animateButton(button){
+    button.animate({opacity: 0}, 70).animate({opacity: 1}, 70);
+}
 
+// generate audio object 
+function audiodObj(color){
+    return new Audio('./sounds/' + color + '.mp3');
+}
